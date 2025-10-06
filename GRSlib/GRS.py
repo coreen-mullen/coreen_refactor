57682ab9f463cf30e04e7c7d45845eb50305ce9c
coreen-mullen <coreenmullen.8052@gmail.com>
2025-10-06 11:22:47 -0600

    edited scoring and grs.py for ensemble.

---
+++ GRSlib/GRS.py
@@ -0,0 +1,293 @@
+from GRSlib.parallel_tools import ParallelTools
+from GRSlib.io.input import Config
+from GRSlib.converters.convert_factory import convert
+from GRSlib.motion.scoring import Scoring, elems, get_desc_count
+from GRSlib.motion.scoring_factory import scoring
+from GRSlib.motion.motion import Gradient, Genetic
+from GRSlib.motion.scoring import Scoring
+from GRSlib.motion.motion import Gradient, Optimize, Create
+import random, copy, os, glob, shutil
+import numpy as np
+
+class GRS:
+    """ 
+    >Big comment goes here explaining what the code does
+    Args:
+        input (str): Optional dictionary or path to input file when using library mode; defaults to 
+                     None for executable use.
+        comm: Optional MPI communicator when using library mode; defaults to None.
+
+    Attributes:
+        pt (:obj:`class` ParallelTools): Instance of the ParallelTools class for helping MPI 
+                                         communication and shared arrays.
+        config (:obj:`class` Config): Instance of the Config class for initializing settings, 
+                                      initialized with a ParallelTools instance.
+        >Update once more of the code structure is fleshed out
+    """
+    def __init__(self, input=None, comm=None, arglist: list=[]):
+        self.comm = comm
+        # Instantiate ParallelTools and Config instances belonging to this GRS instance.
+        # NOTE: Each proc in `comm` creates a different `pt` object, but shared arrays still share 
+        #       memory within `comm`.
+        self.pt = ParallelTools(comm=comm)
+        self.pt.all_barrier()
+        self.config = Config(self.pt, input, arguments_lst=arglist)
+
+        #Set up the generic variables used everwhere
+        self.data = self.config.sections['TARGET'].start_fname
+        desc_types = ["current", "target", "prior"] 
+        self.descriptors = {key: None for key in desc_types}
+
+        #Set up the super functions 
+        self.convert = convert(self.config.sections['BASIS'].descriptor,self.pt,self.config)
+        self.loss_func = scoring(self.config.sections['SCORING'].score_type, self.pt, self.config) # Find out which class to call for loss function
+#       Instantiate other backbone attributes.
+#       self.basis = basis(self.config.sections["BASIS"].descriptor, self.pt, self.config) if "BASIS" in self.config.sections else None
+
+        # Check LAMMPS version if using nonlinear solvers.
+        if (hasattr(self.pt, "lammps_version")):
+            if (self.pt.lammps_version < 20220915):
+                raise Exception(f"Please upgrade LAMMPS to 2022-09-15 or later to use MLIAP based structure searching.")
+
+    def __del__(self):
+        """Override deletion statement to free shared arrays owned by this instance."""
+        self.pt.free()
+        del self
+
+    def __setattr__(self, name: str, value):
+        """
+        Override set attribute statement to prevent overwriting important attributes of an instance.
+        """
+        protected = ("pt", "config")
+        if name in protected and hasattr(self, name):
+            raise AttributeError(f"Overwriting {name} is not allowed; instead change {name} in place.")
+        else:
+            super().__setattr__(name, value)
+
+    def convert_to_desc(self,data):
+        """
+        Accepts a structure (xyz) as input and will return descriptors (D), optionally will convert
+        between file types (xyz=lammps-data, ase.Atoms, etc)
+        """
+        #Pass data to, and do something with the functs of convert
+        #print("Called Convert To Descriptors for %s" % data)
+        descriptors = self.convert.run_lammps_single(data)
+#        if self.config.sections['OUTPUT'].verbosity:
+        if not os.path.exists('%s.npy'%data):
+            np.save('%s.npy'%data.removesuffix(".data"), descriptors)
+            
+        return descriptors
+
+    def set_prior(self,structs):
+        """
+        Accepts a structure (xyz) as input and will return descriptors (D) to be stored and used in conjunction with
+        the target, optionally will convert between file types (xyz=lammps-data, ase.Atoms, etc). This is available 
+        when a single structure cannot reproduce the target.
+        """
+        try:
+            self.descriptors['prior'] = np.load('prior.npy')
+        except:
+            pass
+        for data in structs:
+            prior_desc = self.convert.run_lammps_single(data)
+            try:
+                self.descriptors['prior'] = np.r_[self.descriptors['prior'], prior_desc] #appends arrays along the first axis (row-wise)
+            except:
+                self.descriptors['prior'] = prior_desc
+        np.save('prior.npy', self.descriptors['prior'])
+        
+    def update_start(self,data,str_option):
+        """
+        Used to update the starting structure, usually after a genetic/gradient move has been applied.
+        """
+        #Save the last structure in a meaningful way, check if other data is present already
+        store_id = len(glob.glob(self.config.sections['TARGET'].job_prefix+"*.data"))
+        if store_id==0:
+            data = self.config.sections['TARGET'].start_fname
+        shutil.copyfile(data, self.config.sections['TARGET'].job_prefix + "_%s.data"%store_id)
+
+        if str_option == "Continue":
+            #Take the last state from either genetic/gradient move and continue with next move
+            data = self.config.sections['TARGET'].job_prefix + "_%s.data"%store_id
+        elif str_option == "MinScore":
+            #If the score has decreased, continue. Else, retry last structure.
+            if(self.before_score >= self.after_score):
+                data = self.config.sections['TARGET'].job_prefix + "_%s.data"%store_id
+            else:
+                try:
+                    data = self.config.sections['TARGET'].job_prefix + "_%s.data"%(store_id-1)
+                except:
+                    data = self.config.sections['TARGET'].job_prefix + "_last.data"                  
+        elif str_option == "MaxScore":
+            #If the score has increased, continue. Else, retry last structure.
+            if(self.before_score < self.after_score):
+                data = self.config.sections['TARGET'].job_prefix + "_%s.data"%store_id
+            else:
+                try:
+                    data = self.config.sections['TARGET'].job_prefix + "_%s.data"%(store_id-1)
+                except:
+                    data = self.config.sections['TARGET'].job_prefix + "_last.data"
+#        elif str_option == "Template":
+#            #Call structure builder from template, see James' old code
+#        elif str_option == "Random":
+#            #Call structure buider that creates a random cell of atoms
+        elif str_option == "Reset":
+            #Fallback to the original input strcture
+            data = self.config.sections['TARGET'].start_fname
+        elif str_option == "Last":
+            #Fallback to the last structure before the most recent genetic/gradient move.
+            data = self.config.sections['TARGET'].job_prefix + "_last.data"
+        else:
+            print("You did not specify a continuation condition (Continue, MinScore, MaxScore, Template, Random, Reset, Last), exiting")
+            exit()
+        return data
+
+    def get_score(self,data):
+        """
+        Accepts a structure (xyz) as input and will return descriptors (D), optionally will convert
+        between file types (xyz=lammps-data, ase.Atoms, etc)
+        """
+        #Pass data to, and do something with the functs of scoring
+        if self.config.sections['TARGET'].target_fname == None:
+            print("Provided target descriptors superceed target data file")
+            self.descriptors['target'] = np.load(self.config.sections['TARGET'].target_fdesc)    
+        else:
+            self.descriptors['target'] = self.convert_to_desc(self.config.sections['TARGET'].target_fname)
+        self.descriptors['current'] = self.convert_to_desc(data)
+        
+        if self.descriptors.get('prior',None)==None: 
+            self.set_prior([self.config.sections['TARGET'].start_fname])
+
+        if (np.shape(self.descriptors['current'][1])==np.shape(self.descriptors['target'][1])):
+            #Define scoring method now that descriptors and starting data are available
+            self.score = Scoring(self.pt, self.config, self.loss_func, data, self.descriptors) 
+            score = self.score.get_score() 
+        else:
+            raise RuntimeError(">>> Found unmatched BASIS for target and current descriptors")
+            
+        return score
+    def get_ensemble(self,data):
+        print("Get ensemble called.")
+        print("Using elems:",elems)
+        print("Ensemble Target")
+        scores=None
+        try:
+            scores = self.score.ensemble_score(
+                n_totconfig=10,  # Example value
+                data_path='bcc.data',  # Example value
+                cross_weight=1.0,
+                self_weight=1.0,
+                randomize_comps=False,
+                mincellsize=54,
+                maxcellsize=55,
+                target_comps={'W': 1.0},  # Ensure this is a dictionary
+                min_typ_global='box',
+                soft_strength=0.0,
+                nelements=len(elems),  # Ensure elems is defined
+                n_descs=get_desc_count('coupling_coefficients.yace'),  # Ensure this function is defined
+                mask=None,  # Set this as needed
+                rand_comp=1)  # Ensure this is defined correctly
+            print("Scores returned from ensemble_score:", scores)
+        except Exception as e:
+            print(f"An error occurred while calculating the ensemble score: {e}")
+            return None  # Optionally return None or handle the error as needed
+        if scores is None or len(scores) == 0:
+            print("No scores were calculated returning None")
+            raise RuntimeError("Ensemble score could not be calculated.")
+
+        return scores  # Ensure you return the score
+#""" self.score = Scoring(data, self.current_desc, self.target_desc, self.prior_desc, self.pt, self.config)score = self.score.ensemble_score() return score"""
+
+    def propose_structure(self):
+        """
+        Propose new structure from random, ase, or templates.
+        """
+        @self.pt.single_timeit
+        def propose_structure():
+            print("Called Propose_Structure")
+        propose_structure()
+
+    def genetic_move(self,data):
+        """
+        Hybridize or mutate a structure using a set of moves sampled via a genetic algorithm
+        """
+#        @self.pt.single_timeit
+#        def genetic_move():
+#        genetic_move()
+        #1) Propose a set of structures from templates, ase, or random (or read in a list of ase.Aatoms objects)
+        #2) Score each of the candidates (ase_to_lammps -> run_single)
+        #3) Hybridize, Mutate based on set of rules and probabilities
+        #4) Store socring information with best-of-generation and best-overall isolated
+        #5) Loop until generation limit or scoring residual below threshold
+        print("Called Genetic_Move")
+        if self.current_desc is None:
+            self.current_desc = self.propose_structure()
+        #if data == None:
+            #data = self.propose_structure()
+
+        self.descriptors['current'] = self.convert_to_desc(data)
+        self.descriptors['target'] = self.convert_to_desc(self.config.sections['TARGET'].target_fname) 
+        self.genmove = Genetic(self.pt, self.config, data, self.descriptors) 
+        #Dont want to make a func call the default here since the user will define this?
+        #Need a fallback to provide a good default if a genetic move is called.
+
+#    @self.pt.single_timeit 
+    def gradient_move(self,data):
+        """
+        Accepts a structure (xyz, ase.Atoms) as input and will return updated structure (xyz, ase.Atoms) that 
+        has been modified by motion of atoms on the loss function potential
+        """
+#        @self.pt.single_timeit 
+#        def gradient_move():
+#        gradient_move()
+        #1) Take in target descriptors, convert to moments of descriptor distribution or other loss function
+        #2) Take in current descriptors, convert to moments of descriptor distribution or other loss function
+        #3) Construct a fictitious potential energy surface based on difference in moments or other loss function (self.loss_func)
+        #4) Assemble a LAMMPS input script that overlaps potentials and runs dynamics (self.score)
+        #5) Return an updated structure and scoring on the difference in moments or other loss function (self.gradmove)
+#        print("Called Gradient_Move")
+        
+        if data == None:
+            data = self.propose_structure()
+        self.descriptors['current']= self.convert_to_desc(data)
+        try:
+            self.descriptors['target'] = np.load(self.config.sections['TARGET'].target_fdesc)    
+        except:
+            self.descriptors['target'] = self.convert_to_desc(self.config.sections['TARGET'].target_fname)
+   
+        self.score = Scoring(self.pt, self.config, self.loss_func, data, self.descriptors)  # Set scoring class to assign scores to moves
+        self.gradmove = Gradient(self.pt, self.config, data, self.score) #Set desired motion class with scoring attached
+        if self.config.sections['MOTION'].min_type == 'fire':
+            self.before_score, self.after_score, data = self.gradmove.fire_min()
+        elif self.config.sections['MOTION'].min_type == 'line':
+            self.before_score, self.after_score, data = self.gradmove.line_min()
+        elif self.config.sections['MOTION'].min_type == 'box':
+            self.before_score, self.after_score, data = self.gradmove.box_min()
+        elif self.config.sections['MOTION'].min_type == 'temp':
+            self.before_score, self.after_score, data = self.gradmove.run_then_min()
+        
+        self.write_output()
+        
+        return data
+
+    def baseline_training(self):
+        """
+        Accepts a structure (xyz, ase.Atoms) as input and will return updated structure (xyz, ase.Atoms) that 
+        has been modified by motion of atoms on the loss function potential
+        """
+        @self.pt.single_timeit
+        def baseline_training():
+            #This is more of a 'super' function because it will call many other routines to give the result of a
+            #baseline training set with many (hundreds? thousands?) candidate structures. Should return a score
+            #of the training diversity based on moments of the descriptor distribution.
+            print("Called Baseline_Training")
+        baseline_training()
+
+    def write_output(self):  
+        #Can modfiy to take in an output style and call a specific function, for now it is generic and redundant naming
+#        @self.pt.single_timeit
+        def text_output():
+            store_id = len(glob.glob(self.config.sections['TARGET'].job_prefix+"*.data"))
+            with open("scoring_%s.txt"%self.config.sections['TARGET'].job_prefix, "a") as f:
+                print(store_id,self.before_score, self.after_score, file=f)
+        text_output()
---
+++ GRSlib/motion/scoring.py
@@ -0,0 +1,226 @@
+from GRSlib.Ver0_Files.opt_tools import internal_generate_cell
+from GRSlib.parallel_tools import ParallelTools
+#from GRSlib.motion.lossfunc.moments import LossFunction
+from GRSlib.Ver0_Files.opt_tools import get_desc_count
+#from GRSlib.parallel_tools import ParallelTools
+#from GRSlib.motion.lossfunc.moments import Moments
+#from GRSlib.motion.lossfunc import Gradient
+from GRSlib.converters.sections.lammps_base import Base, _extract_compute_np
+from examples.simple_test.GRS_protocol import GRSModel, GRSSampler
+import lammps, lammps.mliap
+from lammps.mliap.loader import *
+from functools import partial
+import numpy as vnp
+from GRSlib.Ver0_Files.opt_tools import *
+from GRSlib.GRS import *
+#from examples.simple_test.GRS_protocol import *
+#Scoring has to be a class within motion because we want a consistent reference for scores, ans this
+#refrence will be LAMMPS using a constructed potential energy surface from the representation loss function
+n_totconfig = 10
+data_path = 'bcc.data'
+cross_weight =1.000000
+self_weight = 1.000000
+randomize_comps= False # # flag to use randomized compositions for elements in the dictionary: target_comps = {'Cr':1.0 }
+mincellsize = 54
+maxcellsize=55
+target_comps = {'W:1.0'}
+min_typ_global='box' #box or min
+soft_strength=0.0
+elems=get_desc_count('coupling_coefficients.yace',return_elems=True)
+nelements= len(elems)
+n_descs= get_desc_count('coupling_coefficients.yace')
+rand_comp =1
+
+#Scoring has to be a class within motion because we want a consistent reference for scores, and this
+#refrence will be LAMMPS using a constructed potential energy surface from the representation loss function.
+#Sub-classes of Scoring will be versions of this representation loss function (Moments, Entropy, etc), allowing
+#for custom verions to be added without trouble.
+
+class Scoring:
+
+#    def __init__(self, pt, config, data, loss_ff, **kwargs):
+    def __init__(self, pt, config, loss_func, data, descriptors):
+        self.pt = pt #ParallelTools()
+        self.config = config #Config()
+        self.data = data
+        self.descriptors = descriptors
+        self.loss_func = loss_func
+        self.loss_func.__init__(self.pt, self.config, self.descriptors) #Initialize loss function, get ready to send to scoring
+        self.loss_func(self.pt, self.config, self.descriptors) #Call loss function, get ready to send to scoring
+        self.lmp = self.pt.initialize_lammps('log.lammps',0)
+        lammps.mliap.activate_mliappy(self.lmp)
+
+    def construct_lmp(self):
+        #Generates the major components of a lammps script needed for a scoring call
+#        me = self.lmp.extract_setting("world_rank")
+#        nprocs = self.lmp.extract_setting("world_size")
+#        cmds = ["-screen", "none", "-log", "none"]
+#        self.lmp = lammps(cmdargs = cmds)
+        self.lmp = self.pt.initialize_lammps('log.lammps',0)
+        lammps.mliap.activate_mliappy(self.lmp)
+        construct_string=\
+        """
+        units metal
+        atom_style atomic
+        read_data {}
+        pair_style hybrid/overlay soft 1.0 mliap model mliappy LATER descriptor ace coupling_coefficients.yace
+        pair_coeff * * soft {}
+        pair_coeff * * mliap {}
+        neighbor 2.3 bin
+        neigh_modify one 10000
+        thermo 10
+        thermo_style custom step etotal temp press
+        """
+        init_lmp=construct_string.format(self.data, self.config.sections["MOTION"].soft_strength, (" ".join(str(x) for x in self.config.sections['BASIS'].elements)))
+        #TODO make the possibility to import any reference potential to be used with the mliap one
+        self.lmp.commands_string(init_lmp)
+        lammps.mliap.load_model(self.loss_func)
+        self.lmp.command("run 0")
+              
+    def get_atomic_energies(self):
+        #Return as array per-atom energies for the set of potentials applied
+        lammps.mliap.activate_mliappy(self.lmp)
+        self.construct_lmp()
+        self.lmp.command("compute peatom all pe/atom")
+        self.lmp.command("run 0")
+        num_atoms = self.lmp.extract_global("natoms")
+        atom_energy = _extract_compute_np(self.lmp, "peatom", 0, 2, (num_atoms, 1))
+#        del self.lmp
+        return atom_energy
+
+    def get_norm_forces(self):
+        #Return as array per-atom forces 
+        self.construct_lmp()
+        self.lmp.command("compute fatom all property/atom fx fy fz")
+        self.lmp.command("run 0")
+        num_atoms = self.lmp.extract_global("natoms")
+        atom_forces = _extract_compute_np(self.lmp, "fatom", 0, 2, (num_atoms, 3))        
+#        del self.lmp
+        return atom_forces
+
+    def get_score(self):
+        self.construct_lmp()
+        self.lmp.command("run 0")
+        score = self.lmp.get_thermo("pe") # potential energy
+#        del self.lmp
+        return score
+
+    def add_cmds_before_score(self,string):
+        self.construct_lmp()
+        before_score = self.get_score()
+        self._extract_commands(string)
+        self.lmp.commands_string("run 0")
+        after_score = self.lmp.get_thermo("pe") # potential energy
+#        del self._lmp
+        return before_score, after_score
+
+    def _extract_commands(self,string):
+        #Can be given a block of text where it will split them into individual commands
+        add_lmp_lines = [x for x in string.splitlines() if x.strip() != '']
+        for line in add_lmp_lines:
+            self.lmp.command(line)
+#internal generate cell is only used in scoring.py but defined in opt_tools + gsqsmodel only in scoring.py currently
+#class ensemble_score(): #will take in the target, and compare it to multiple generated structures -- look at fitsnap sections
+# min and maxatoms the same as maxcellsize?
+# target_comps is in input
+#numelements = num types? 
+    def ensemble_score(self, n_totconfig, data_path, cross_weight, self_weight, randomize_comps, mincellsize, maxcellsize, target_comps, min_typ_global, soft_strength, nelements, n_descs, mask, rand_comp):
+        if mask == None:
+            mask = range(n_descs)
+        self.mask = mask  # Generates the multiple structures
+        scores = []  # Initialize a list to store scores
+        
+        print(f"Starting ensemble_score with {n_totconfig} configurations.")
+
+        for i in range(1, n_totconfig + 1):
+            print(f"Configuration {i}/{n_totconfig} - Using indices: {mask}")
+            g = internal_generate_cell(i, desired_size=vnp.random.choice(range(mincellsize, maxcellsize)), template=None, desired_comps=target_comps, use_template=None, min_typ=min_typ_global, soft_strength=soft_strength)
+            print(g)
+            print(f"Cell generated for configuration {i}:{g}") 
+
+            em = GRSModel(nelements, n_descs, mask=mask)
+            sampler = GRSSampler(em, g)
+            em.K_cross = cross_weight
+            em.K_self = self_weight
+
+            print(f"Running minimization for configuration {i}.")  # Debugging line
+        # Run the minimization process
+            sampler.run("minimize 1e-6 1e-6 1000 10000")
+
+            print(f"Minimization completed for configuration {i}. Writing data.")  # Debugging line
+        # Write the data to a file
+            sampler.run("write_data %s/sample.%i.dat " % (data_path, i))
+
+            print(f"Data written for configuration {i}. Updating model.")  # Debugging line
+            sampler.update_model()  # Updating the model combines generated structures
+
+        # Calculate the score for the current configuration
+            score = self.get_score(g)  # Pass the generated structure to get_score
+            print(f"Score for configuration {i}: {score}")  # Debugging line
+
+            if score is None:
+                print(f"Score for configuration {i} is None.")  # Debugging line
+
+            scores.append(score)  # Append the score to the list
+
+        print("Final scores list:", scores)  # Debugging line
+        return scores  # Return the list of scores
+"""def ensemble_score(self, n_totconfig, data_path, cross_weight, self_weight, randomize_comps, mincellsize, maxcellsize, target_comps, min_typ_global, soft_strength, nelements, n_descs, mask, rand_comp):
+        self.mask = mask  # Generates the multiple structures
+        scores = []  # Initialize a list to store scores
+        print(f"Starting ensemble_score with {n_totconfig} configs.")
+        for i in range(1, n_totconfig + 1):
+            print(i, "/", n_totconfig, "Using indices:", mask)
+
+            if not randomize_comps:
+                g = internal_generate_cell(i, desired_size=vnp.random.choice(range(mincellsize, maxcellsize)), template=None, desired_comps=target_comps, use_template=None, min_typ=min_typ_global, soft_strength=soft_strength)
+            else:
+                target_comps_rnd = rand_comp(target_comps)  # Randomize compositions
+                g = internal_generate_cell(i, desired_size=vnp.random.choice(range(mincellsize, maxcellsize)), template=None, desired_comps=target_comps_rnd, use_template=None, min_typ=min_typ_global, soft_strength=soft_strength)
+
+            em = GRSModel(nelements, n_descs, mask=mask)
+            sampler = GRSSampler(em, g)
+            em.K_cross = cross_weight
+            em.K_self = self_weight
+
+        # Run the minimization process
+            sampler.run("minimize 1e-6 1e-6 1000 10000")
+
+        # Write the data to a file
+            sampler.run("write_data %s/sample.%i.dat " % (data_path, i))
+
+            sampler.update_model()  # Updating the model combines generated structures
+
+        # Calculate the score for the current configuration
+            score = self.get_score()  # Assuming get_score is defined to return the score for the current state
+            if score is None:
+                print(f"Score for configuration {i} is None.")
+            scores.append(score)  # Append the score to the list
+        print("Final scores list:", scores)
+        return scores  # Return the list of scores
+"""
+"""    def ensemble_score(self, n_totconfig, data_path,cross_weight, self_weight, randomize_comps, mincellsize, maxcellsize, target_comps, min_typ_global,soft_strength, nelements,n_descs,mask,rand_comp): 
+        self.mask=mask#generates the multiple structures -- needs internal generate cell , some of these should be defined in the input file like n_totconfig if they choose multiple and the crossweight and self weigths
+        i = 1
+        while i <= n_totconfig: 
+            print(i,"/",n_totconfig,"Using indicies :",mask)
+        if not randomize_comps:
+            g = internal_generate_cell(i,desired_size=vnp.random.choice(range(mincellsize,maxcellsize)),template=None,desired_comps=target_comps,use_template=None,min_typ=min_typ_global,soft_strength=soft_strength)
+        else:
+            target_comps_rnd = rand_comp(target_comps) #randomize_comp in input? and target_comps in input?
+            g = internal_generate_cell(i,desired_size=vnp.random.choice(range(mincellsize,maxcellsize)),template=None,desired_comps=target_comps_rnd,use_template=None,min_typ=min_typ_global,soft_strength=soft_strength)
+        em=GRSModel(nelements,n_descs,mask=mask) 
+        sampler = GRSSampler(em, g)
+        em.K_cross = cross_weight
+        em.K_self = self_weight
+        #min type
+        sampler.run("minimize 1e-6 1e-6 1000 10000")
+        
+        sampler.run("write_data %s/sample.%i.dat " % (data_path, i))
+        
+        sampler.update_model() #updating the model is how all of the generated structures get combined into one.
+    
+"""
+# total number of configurations
+#n_totconfig = int(sys.argv[2])
+               # self.lmp.command(line)
